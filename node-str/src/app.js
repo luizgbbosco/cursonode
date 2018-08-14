a66363beb7c11e5d972dcaa7fae922b610446d1f
luizgbbosco <luizgbbosco@gmail.com>
2018-08-14 15:03:49 -0300

    primeiro commit

---
+++ node-str/src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+//Carregar rotas
+const indexRoute = require('./routes/indexRoute');
+const productRoute = require('./routes/productRoute');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
+
+module.exports = app;
